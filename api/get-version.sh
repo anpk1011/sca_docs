70e94e0f8b9e9c7a1256ab3111f567319a79b3c1
an pham <an.phamky1234@gmail.com>
2025-12-11 12:26:46 +0700

    ninth

--- api/get-version.sh
+++ api/get-version.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 PROJECT_ID=$1
-API="https://192.168.1.227/api/projects/$PROJECT_ID/versions"
-OUT="projects.json"
+API="https://192.168.1.227:443/api/projects/e31c43d8-0fd8-411f-825e-8ae020531ab2/versions"
+OUT="versions.json"
 
-curl --insecure -X GET "$API" \
-  -H "Authorization: Bearer $(jq -r .bearerToken token.json)" \
-  -H "Accept: application/vnd.blackducksoftware.project-detail-7+json"\
-  | jq
+curl --insecure -X GET "$API"  \
+  -H "Authorization: Bearer $(jq -r .bearerToken token.json)"  \
+  -H "Accept: application/vnd.blackducksoftware.project-detail-5+json"  \
+  | jq '.items | map({version_name: .versionName, version_key: (._meta.href | sub(".*/versions/"; ""))})' > "$OUT"
+
+
